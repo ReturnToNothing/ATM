0f19de6eaa0a1e9d37b22d1d4c57da6b36e54a86
ReturnToNothing <ericdesouza24@gmail.com>
2025-03-03 15:07:57 +0000

    Introduced sub state enums + account enums (although you should be moved into Status.java instead of States.java.)

--- src/classes/java/com/atm/States.java
+++ src/classes/java/com/atm/States.java
@@ -2,10 +2,18 @@ package com.atm;
 
 public enum States
 {
+    // Model's internal state
     ACCOUNT_NO,
     PASSWORD_NO,
     LOGGED_IN,
 
+    // Model's internal sub-state
     DEFAULT,
     WITHDRAWAL,
+    DEPOSIT,
+    BALANCE,
+
+    // Account's internal type; determines the benefits of providing additional features or certain restrictions.
+    BASIC,
+    PRIME
 }
