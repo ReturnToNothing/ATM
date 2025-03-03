c381eabd6f18e8eff95024345fe50853f8adc7ae
ReturnToNothing <ericdesouza24@gmail.com>
2025-03-03 14:57:58 +0000

    Added two different types of Accounts; Basic and Prime, each providing different benefits and restrictions.
    Added restriction logic based on the account's type.
    
    Using "Stats" enum for providing enum feedback for the model.
    
    Might handle the logic to the bank instead of the object.

--- src/classes/java/com/atm/Account.java
+++ src/classes/java/com/atm/Account.java
@@ -5,32 +5,59 @@ public class Account
     private final int number;
     private final int password;
     private double balance;
+    private States type;
 
-    public Account(int number, int password)
+    public Account(int number, int password, States type)
     {
         this.number = number;
         this.password = password;
-        this.balance = 0.00f;
+        this.balance = 100.00f;
+        this.type = type;
     }
 
-    public boolean withdraw(double amount)
+    public Status withdraw(double amount)
     {
+        double limit = 100;
+        boolean overdraft = false;
+
         amount = Math.abs(amount);
-        if (this.balance >= amount)
+
+        if (this.type.equals(States.PRIME))
+        {
+            limit = 1000;
+            overdraft = true;
+        }
+
+        // Check if the requested withdrawal exceeds the limit.
+        if (amount > limit)
+        {
+            return Status.EXCEEDS_LIMIT; // Withdrawal amount exceeds the allowed limit.
+        }
+
+        // Verify if the withdrawal can be processed, bypassed if overdraft is allowed.
+        if (this.balance >= amount || overdraft)
         {
             this.balance -= amount;
-            return true;
+            return Status.SUCCESSFUL;
+        }
+        else
+        {
+            return Status.INSUFFICIENT_FUNDS;
+        }
+    }
+
+    public boolean deposit(double amount)
+    {
+        amount = Math.abs(amount);
+        if (amount <= 1000)
+        {
+            this.balance += amount;
+            return false;
         }
         else
         {
             return false;
         }
-    }
-
-    public void deposit(double amount)
-    {
-        amount = Math.abs(amount);
-        this.balance += amount;
     }
 
     public int getNumber()
